72d345e2c37e9ab137af6ba2d0b8f347bb4da055
Asraf beta 6 <asraf.beta6@gmail.com>
2025-12-21 23:48:02 +0530

    Further manual changes (repeated)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body 
         className={`${poppins.className} text-white antialiased min-h-screen`}
-        style={{ background: 'linear-gradient(to bottom right, #40E0D0, #40B5AD)' }}
+        style={{ background: 'linear-gradient(to bottom right, #FF00B3, #FF00B3)' }}
       >
         <CursorAura /> {/* Add the aura component */}
         {children}
