431e89d62ed39ae1314c830f2d32942c341abcab
Mohammed Asraf <162663805+Mohammed-Asraf@users.noreply.github.com>
2026-02-12 11:27:29 +0530

    Update Footer.tsx
    Footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-white/10 backdrop-blur-md text-white py-6 text-center">
       <div className="container mx-auto px-4 flex justify-center items-center">
-        <p>Designed and Crafted by AsRaf!! 🌌👽</p>
+        <p>Built from CuriositYY 🌌👽</p>
       </div>
     </footer>
   );
